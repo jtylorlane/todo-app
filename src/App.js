bb4e3fbde64f6fd0bcd4b86bf69f8b5aa8bb382c
jtylorlane <jtylorlane@yahoo.com>
2022-07-27 14:32:34 -0500

    changes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
 import logo from './logo.svg';
 import './App.css';
+import React, {Component} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component  {
+  constructor(props){
+   super(props);
+   this.state = {isClicked: false, todos: [], text: ''}
+   }
+  
+  handleOnChange = e => {
+    this.setState({
+      text: e.target.value
+    })
+  }
+    onClickHandler = e => {
+      console.log(this.state.text);
+      //populate array of todos with text
+      //.push()
+      const text = this.state.text;
+      const newState = [...this.state.todos]
+      console.log(this.state.todos);
+      newState.push(text);
+      console.log(newState);
+      //set the state
+      this.setState({
+        text: '',
+        todos: newState // newstate goes here, how do i annote it, I'm so dumb it was literally just newState
+      })
+    }
+  
+  render() {
+    return (
+      <div className="App">
+       <h1>ToDo</h1>
+       <input value={this.state.text} onChange={this.handleOnChange}/>
+       <p>{this.state.text}</p>
+       <button onClick = {this.onClickHandler}>add</button>
+      </div>
+    );
+  }
+  
 }
 
 export default App;
